1b8ccff7184a5511fe6c4a9ca4d941ac4e04099e
raman <mailtorraman@gmail.com>
2020-08-21 10:43:12 +0530

    added canary etc

--- bin/env.sh
+++ bin/env.sh
@@ -1,1 +1,6 @@
 alias k="kubectl"
+export PATH=$PATH:$HOME/.linkerd2/bin:$HOME/dev/researchpoc/bin
+alias kgp="kubectl get pods"
+alias kgs="kubectl get service"
+alias kgd="kubectl get deployment"
+alias kg="kubectl get "
